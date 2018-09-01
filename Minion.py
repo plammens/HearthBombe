973d994e4c2c7180b56a29f947894612cf1fe4e3
Paolo Lammens <lammenspaolo@gmail.com>
2018-09-01 15:32:47 +0200

    Add attack and health @properties to 'Minion'
    Also, renamed 'defense' to 'attack'.

--- Minion.py
+++ Minion.py
@@ -3,13 +3,38 @@ from Card import Card
 
 
 class Minion(Card):
-    def __init__(self, mana, attack, defense):
+    def __init__(self, mana, attack, health):
         super(Minion, self).__init__(mana)
 
         self.attack = attack
-        self.defense = defense
+        self.health = health
 
         Game.player_battlefield.append(self)
 
     def __del__(self):
         Game.player_battlefield.remove(self)
+
+    @property
+    def attack(self):
+        return self._attack
+
+    @attack.setter
+    def attack(self, val):
+        if type(val) is not int or val < 0:
+            raise ValueError
+
+        self._attack = val
+
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, val):
+        if type(val) is not int:
+            raise TypeError
+
+        self._health = val
+
+        if self.health <= 0:
+            del self
