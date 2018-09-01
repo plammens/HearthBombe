c8432d80509f72aad5ae45d9c5af2ddeea982d99
Paolo Lammens <lammenspaolo@gmail.com>
2018-09-01 09:21:28 +0200

    Add class 'Card'

---
+++ Card.py
@@ -0,0 +1,17 @@
+class Card:
+    def __init__(self, mana):
+        self.mana = mana
+
+    @property
+    def mana(self):
+        return self._mana
+
+    @mana.setter
+    def mana(self, val: int):
+        if type(val) is not int or val < 0:
+            raise ValueError
+
+        if not hasattr(self, '_mana'):
+            self._mana = {'base': val, 'current': val}
+
+        self._mana['current'] = val
