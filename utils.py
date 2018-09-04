aea24d28da60ab3fcea1c6f5d7f7a9c840f78962
Paolo Lammens <lammenspaolo@gmail.com>
2018-09-04 13:05:48 +0200

    feat: Give Hand and Battlefield their own class
    Ensuring every card's player/owner/controller is set upon being added to the Hand/Battlefield.

---
+++ utils.py
@@ -0,0 +1,60 @@
+"""
+TODO: Hand, Battlefield: change RuntimeError to more specific exception
+TODO: make Player_Property metaclass for Hand and Battlefield
+TODO: separate from utils?
+"""
+
+
+# from Card import Card
+# from Minion import Minion
+
+
+class Callable_List(list):
+    def __init__(self, *args):
+        list.__init__(self, *args)
+
+    def append(self, val):
+        if callable(val):
+            list.append(self, val)
+        else:
+            raise TypeError
+
+
+class Hand(list):
+    def __init__(self, holder) -> None:
+        list.__init__(self)
+        self.holder = holder
+
+    def initialize(self, *cards):
+        self.clear()
+        for card in cards:
+            list.append(self, card)
+            card.player = self.holder
+            card.owner = self.holder
+
+    def remove(self, card):
+        try:
+            list.remove(self, card)
+        except ValueError:
+            raise RuntimeError("Removing missing card from hand!")
+
+
+class Battlefield(list):
+    def __init__(self, player) -> None:
+        list.__init__(self)
+        self.controller = player
+
+    def initialize(self, *minions):
+        self.clear()
+        for minion in minions:
+            self.add(minion)
+
+    def add(self, minion):
+        list.append(self, minion)
+        minion.controller = self.controller
+
+    def remove(self, minion):
+        try:
+            list.remove(self, minion)
+        except ValueError:
+            raise RuntimeError("Removing missing minion from battlefield!")
