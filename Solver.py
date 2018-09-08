fbf52f232fe44c34d9f663f24a6f18011a4cd792
Paolo Lammens <lammenspaolo@gmail.com>
2018-09-08 20:24:01 +0200

    feat: Create Solver module

---
+++ Solver.py
@@ -0,0 +1,10 @@
+def solve(objective: str):
+    """Algorithm for solving puzzles"""
+
+    solved = False
+    while not solved:
+        # gather possibilities
+        # expand possibilities
+        # check if target is met
+        pass
+
